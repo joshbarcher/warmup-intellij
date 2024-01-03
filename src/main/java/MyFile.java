f6d1d3229781f331401659887bd9111fc3c79d05
Josh Archer <jarcher@greenriver.edu>
2024-01-03 15:55:20 -0800

    Revert "Added a printMessage() method."
    This reverts commit 99ab1718

--- src/main/java/MyFile.java
+++ src/main/java/MyFile.java
@@ -2,20 +2,6 @@ public class MyFile
 {
     public static void main(String[] args)
     {
-        printHeader();
-        printMessage("hello, world!");
-
-        printHeader();
-        printMessage("by: Josh");
-    }
-
-    private static void printMessage(String message)
-    {
-        System.out.println(message);
-    }
-
-    private static void printHeader()
-    {
-        System.out.println("******************************");
+        System.out.println("Hello, world!");
     }
 }
